14f0a00dc66353dc31b1a41e6abcd939e657936c
NiharSadhu <W0810177@myscc.ca>
2023-02-16 04:53:29 -0500

    update for SeedData

---
+++ TowelsMvc/Models/SeedData.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TowelsMvc.Data;
+using System;
+using System.Linq;
+
+namespace TowelsMvc.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ApplicationDbContext>>()))
+            {
+                // Look for any movies.
+                if (context.Towel.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Towel.AddRange(
+                    new Towel
+                    {
+                        Material = "Nylon",
+                        Size = "M",
+                        Price = 100.00M,
+                        Design = "Nice",
+                        Softness = "Very",
+                        Durability = "a year"
+                    }
+
+                    
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
