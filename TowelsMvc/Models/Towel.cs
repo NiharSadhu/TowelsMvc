0d7c7438dfebafc05880c3f60015d535c4bbbce1
NiharSadhu <W0810177@myscc.ca>
2023-02-16 02:34:12 -0500

    update

---
+++ TowelsMvc/Models/Towel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TowelsMvc.Models
+{
+    public class Towel
+    {
+        public int Id { get; set; }
+        public string Material { get; set; }
+        public string Size { get; set; }
+        public decimal Price { get; set; }
+
+        public string Design { get; set; }
+
+        public string Softness { get; set; }
+
+        public string Durability { get; set; }
+
+    }
+}
